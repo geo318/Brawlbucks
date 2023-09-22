c88223f302b36365c74d1e4c4816d1c1ef045e1f
geo318 <geo.lomidze@gmail.com>
2023-09-22 12:40:39 +0400

    chore: make website more fit to xl screen size

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,23 +1,23 @@
-import { useEmailForm } from '@src/hooks/useEmailForm'
+import { useEmailForm } from "@src/hooks/useEmailForm";
 
 export const EmailForm = () => {
   const { props, formData, message, isLoading, handleSubmit, info } =
-    useEmailForm()
+    useEmailForm();
   return (
     <>
       {message.error && (
-        <p className='text-red-500 border border-red-300 rounded-md p-3'>
+        <p className="text-red-500 border border-red-300 rounded-md p-3">
           ⚠️ {message.error}
         </p>
       )}
       {message.success && (
-        <p className='text-green-500 font-bold border border-green-300 rounded-md p-3 mt-5'>
+        <p className="text-green-500 font-bold border border-green-300 rounded-md p-3 mt-5">
           ✅ {message.success}
         </p>
       )}
       <form
-        method='post'
-        className='grid grid-cols-2 gap-6 mt-8'
+        method="post"
+        className="grid grid-cols-2 gap-6 mt-8"
         onSubmit={handleSubmit}
       >
         {(Object.entries(formData) as [keyof typeof formData, string][]).map(
@@ -25,39 +25,39 @@ export const EmailForm = () => {
             <div
               key={key}
               className={`flex flex-col gap-2 ${
-                info[key].required ? 'lg:col-span-1 col-span-2' : 'col-span-2'
+                info[key].required ? "lg:col-span-1 col-span-2" : "col-span-2"
               }`}
             >
-              <label className='capitalize font-bold text-sm'>
+              <label className="capitalize font-bold text-sm">
                 {key}
-                {info[key].required ? '*' : ''}:
+                {info[key].required ? "*" : ""}
               </label>
-              {key !== 'message' ? (
+              {key !== "message" ? (
                 <input
                   {...props(key)}
-                  className='p-3 border border-gray-900 rounded-lg focus:outline-[#FF0D0D]'
+                  className="p-3 border border-gray-900 rounded-lg focus:outline-[#FF0D0D]"
                 />
               ) : (
                 <textarea
                   {...props(key)}
-                  className='border border-gray-900 rounded-lg p-3 focus:outline-[#FF0D0D]'
+                  className="border border-gray-900 rounded-lg p-3 focus:outline-[#FF0D0D]"
                   rows={5}
                 />
               )}
             </div>
-          )
+          ),
         )}
 
         <button
-          type='submit'
-          className='lg:text-white lg:bg-[#FF0D0D] text-black bg-white lg:hover:bg-[#cf3d3d] hover:bg-slate-200 py-3 text-sm font-bold rounded-lg col-span-2 disabled:opacity-50 disabled:cursor-default'
+          type="submit"
+          className="lg:text-white lg:bg-[#FF0D0D] text-black bg-white lg:hover:bg-[#cf3d3d] hover:bg-slate-200 py-3 text-sm font-bold rounded-lg col-span-2 disabled:opacity-50 disabled:cursor-default"
           disabled={isLoading}
         >
           Submit
         </button>
       </form>
     </>
-  )
-}
+  );
+};
 
-export default EmailForm
+export default EmailForm;
--- src/hooks/useEmailForm.ts
+++ src/hooks/useEmailForm.ts
@@ -1,93 +1,93 @@
-import emailjs from '@emailjs/browser'
-import { useState, type FormEvent, type ChangeEvent } from 'react'
+import emailjs from "@emailjs/browser";
+import { useState, type FormEvent, type ChangeEvent } from "react";
 
 export const useEmailForm = () => {
   const initialValues = {
-    name: '',
-    email: '',
-    phone: '',
-    message: '',
-  } as const
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  } as const;
 
-  const [formData, setFormData] = useState(initialValues)
-  const [message, setMessage] = useState({ error: '', success: '' })
-  const [isLoading, setIsLoading] = useState(false)
+  const [formData, setFormData] = useState(initialValues);
+  const [message, setMessage] = useState({ error: "", success: "" });
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    })
-  }
+    });
+  };
 
   const info = {
     name: {
-      pattern: '.+{30}',
+      pattern: ".+{30}",
       required: true,
-      placeholder: 'your name',
+      placeholder: "your name",
     },
     email: {
-      pattern: '[a-zA-Z0-9.]+@[a-zA-Z0-9.]+.[a-zA-Z0-9]+',
+      pattern: "[a-zA-Z0-9.]+@[a-zA-Z0-9.]+.[a-zA-Z0-9]+",
       required: true,
-      placeholder: 'some@email.com',
+      placeholder: "some@email.com",
     },
     phone: {
-      pattern: '[0-9]{8,13}',
+      pattern: "[0-9]{8,13}",
       required: false,
-      placeholder: '999999999',
+      placeholder: "999999999",
     },
     message: {
-      pattern: '[a-zA-Z0-9]+',
+      pattern: "[a-zA-Z0-9]+",
       required: false,
-      placeholder: 'write something here',
+      placeholder: "write something here",
     },
-  } as const
+  } as const;
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setIsLoading(true)
+    e.preventDefault();
+    setIsLoading(true);
 
     emailjs.send(
-      'service_gvc6531',
-      'template_993zflo',
+      "service_gvc6531",
+      "template_993zflo",
       formData,
-      'kAHuxuzadrgkxtfmO'
-    )
+      "kAHuxuzadrgkxtfmO",
+    );
 
     emailjs
       .send(
-        'service_gvc6531',
-        'template_vvkp9qm',
+        "service_gvc6531",
+        "template_vvkp9qm",
         formData,
-        'kAHuxuzadrgkxtfmO'
+        "kAHuxuzadrgkxtfmO",
       )
       .then(
         () => {
-          setMessage({ error: '', success: 'Email sent successfully!' })
-          setFormData(initialValues)
-          setIsLoading(false)
-          return
+          setMessage({ error: "", success: "Email sent successfully!" });
+          setFormData(initialValues);
+          setIsLoading(false);
+          return;
         },
         () => {
-          setMessage({ error: 'Something went wrong...', success: '' })
-          setIsLoading(false)
-          return
-        }
-      )
-  }
+          setMessage({ error: "Something went wrong...", success: "" });
+          setIsLoading(false);
+          return;
+        },
+      );
+  };
 
   const props = (key: keyof typeof formData) => ({
-    type: key === 'email' ? 'email' : 'text',
+    type: key === "email" ? "email" : "text",
     name: key,
     value: formData[key],
     required: info[key].required,
     onChange: handleChange,
     pattern: info[key].pattern,
     placeholder: info[key].placeholder,
-  })
+  });
 
   return {
     props,
@@ -96,5 +96,5 @@ export const useEmailForm = () => {
     isLoading,
     handleSubmit,
     info,
-  }
-}
+  };
+};
